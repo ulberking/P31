c699bb8b1980797ff60b4d729c6517bf9d25e209
ulberking <83320350+ulberking@users.noreply.github.com>
2021-06-27 22:51:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var rectangle
+function setup() {
+  createCanvas(800, 800);
+   var arr = [10, 20, -26, -93, -34, 43, 30, 40, 50, 55, 59, 67, 78, 49];
+  for (var i = arr.length-1; i > -1; i = i - 1) {
+    var a = arr[i]
+    console.log(a);
+  } 
+
+}
+
+function draw() {
+  background("black");
+
+}
